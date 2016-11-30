c24aa72ce967be6149e1ae070afdb9f28aaee022
Pedro Souza <pedro@vencis.com.br>
2016-11-30 16:01:16 -0200

    Refactoring app and fetching videos from youtube search api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDNsnkrTTAHNMTqy596EEpul8fdgSsYbgU';
 
-//create a new component
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
-};
+class App extends Component {
 
-//put the component's html on the page
+    constructor(props){
+        super(props);
+
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'guns'}, videos => {
+            this.setState({videos});
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+
+}
+
 ReactDOM.render(<App/>, document.querySelector('.container'));
